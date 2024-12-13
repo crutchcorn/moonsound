9ba3b9098349b553c10d82543dfba8f49290c277
Corbin Crutchley <git@crutchcorn.dev>
2024-12-13 11:21:53 -0800

    feat: add theme change listener

--- src/app/components/under-titlebar.component.ts
+++ src/app/components/under-titlebar.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from "@angular/core";
+import { Component, effect, input } from "@angular/core";
 import { platform } from '@tauri-apps/plugin-os';
 import { getCurrentWindow } from '@tauri-apps/api/window';
 import { injectQuery } from "@tanstack/angular-query-experimental";
+import { listen, TauriEvent } from "@tauri-apps/api/event"
 
 @Component({
     selector: "app-under-titlebar-macos",
@@ -44,4 +45,13 @@ export class UnderTitlebar {
         queryKey: ['theme'],
         queryFn: async () => await getCurrentWindow().theme() ?? "dark"
     }))
+
+    _themeChange = effect((onCleanup) => {
+        let cleanup = () => {};
+        listen(TauriEvent.WINDOW_THEME_CHANGED, () => {
+            this.theme.refetch();
+        }).then((c) => cleanup = c);
+        onCleanup(() => cleanup());
+    })
+
 }
