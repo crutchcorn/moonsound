404db47a1eabffaa5565f857d31e793f78101bc7
Corbin Crutchley <git@crutchcorn.dev>
2024-12-13 16:32:12 -0800

    chore: refactor thread_local to use AppData

--- src-tauri/src/music/commands.rs
+++ src-tauri/src/music/commands.rs
@@ -14,7 +14,7 @@ pub fn play(app: AppHandle, path: &str, state: State<'_, Mutex<AppData>>) -> Res
     let mut state = state.lock().unwrap();
     state.currently_playing_file_path = Some(path.to_string());
 
-    core::play_audio(path)?;
+    core::play_audio(&state, path)?;
     app.emit("SERVER_SYNC_EVENT", "").unwrap();
     Ok(())
 }
@@ -24,38 +24,43 @@ pub fn stop(app: AppHandle, state: State<'_, Mutex<AppData>>) {
     let mut state = state.lock().unwrap();
     state.currently_playing_file_path = None;
 
-    core::stop();
+    core::stop(&state);
     app.emit("SERVER_SYNC_EVENT", "").unwrap();
 }
 
 #[tauri::command]
-pub fn set_volume(app: AppHandle, volume: f32) {
-    core::set_volume(volume);
+pub fn set_volume(app: AppHandle, volume: f32, state: State<'_, Mutex<AppData>>) {
+    let state = state.lock().unwrap();
+    core::set_volume(&state, volume);
     app.emit("SERVER_SYNC_EVENT", "").unwrap();
 }
 
 #[tauri::command]
-pub fn set_speed(app: AppHandle, speed: f32) {
-    core::set_speed(speed);
+pub fn set_speed(app: AppHandle, speed: f32, state: State<'_, Mutex<AppData>>) {
+    let state = state.lock().unwrap();
+    core::set_speed(&state, speed);
     app.emit("SERVER_SYNC_EVENT", "").unwrap();
 }
 
 #[tauri::command]
-pub fn seek_to(app: AppHandle, position: std::time::Duration) -> Result<(), String> {
-    core::seek_to(position)?;
+pub fn seek_to(app: AppHandle, position: std::time::Duration, state: State<'_, Mutex<AppData>>) -> Result<(), String> {
+    let state = state.lock().unwrap();
+    core::seek_to(&state, position)?;
     app.emit("SERVER_SYNC_EVENT", "").unwrap();
     Ok(())
 }
 
 #[tauri::command]
-pub fn pause(app: AppHandle) {
-    core::pause();
+pub fn pause(app: AppHandle, state: State<'_, Mutex<AppData>>) {
+    let state = state.lock().unwrap();
+    core::pause(&state);
     app.emit("SERVER_SYNC_EVENT", "").unwrap();
 }
 
 #[tauri::command]
-pub fn resume(app: AppHandle) {
-    core::resume();
+pub fn resume(app: AppHandle, state: State<'_, Mutex<AppData>>) {
+    let state = state.lock().unwrap();
+    core::resume(&state);
     app.emit("SERVER_SYNC_EVENT", "").unwrap();
 }
 
@@ -66,6 +71,7 @@ pub fn get_redux_store_state(state: State<'_, Mutex<AppData>>) -> core::PlayerSt
 }
 
 #[tauri::command]
-pub fn get_position() -> std::time::Duration {
-    core::get_position()
+pub fn get_position(state: State<'_, Mutex<AppData>>) -> std::time::Duration {
+    let state = state.lock().unwrap();
+    core::get_position(&state)
 }
--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -1,6 +1,20 @@
-use serde::Serialize;
+use rodio::{OutputStream, OutputStreamHandle, Sink};
 
-#[derive(Default, Serialize)]
 pub struct AppData {
     pub currently_playing_file_path: Option<String>,
+    pub sink: Sink,
+    pub stream_handle: OutputStreamHandle,
+}
+
+impl Default for AppData {
+    fn default() -> Self {
+        let (_stream, stream_handle )= OutputStream::try_default().unwrap();
+        let sink: Sink = Sink::try_new(&stream_handle).unwrap();
+
+        Self {
+            currently_playing_file_path: None,
+            sink,
+            stream_handle,
+        }
+    }
 }
